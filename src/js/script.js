db2ecd04eff9ef2e8bcc39c942b0d7804fc3fd74
João Nodari <joaonodari2017@hotmail.com>
2024-02-07 22:40:37 -0300

    Asas Brasileiras

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+const botaoAlterarTema = window.document.getElementById("botao-alterar-tema")
+
+const body = window.document.querySelector("body")
+const imagemBotaoTrocaDeTema = window.document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener ("click", () => {
+    const modoClaroAtivo = body.classList.contains("modo-claro")
+    if (modoClaroAtivo) {
+        body.classList.remove("modo-claro")
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/moon.png")
+    } else {
+        body.classList.add("modo-claro")
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/img/sun.png")
+    }
+})
